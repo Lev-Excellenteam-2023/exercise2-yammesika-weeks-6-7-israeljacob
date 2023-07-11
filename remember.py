bd6a05570db0f023f60249a60903e050ed949032
Yisrael jacob <yisraeljakob@gmail.com>
2023-07-11 08:06:35 +0300

    6.4 exercise

---
+++ remember.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+
+def encrypted_message(path: str) -> str:
+    """
+    Extracts an encrypted message from an image.
+
+    Parameters:
+        path (str): The path to the image file.
+
+    Returns:
+        str: The extracted encrypted message.
+    """
+    image = Image.open(path)
+    width, height = image.size
+    return "".join(chr(y) for x in range(width) for y in range(height) if image.getpixel((x, y)) == 1)
+
+
+print(encrypted_message('C:\\Users\\yisra\\Downloads\\code.png'))
+# Place gunpowder beneath the House of Lords. 11/05/1605
